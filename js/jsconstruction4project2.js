a6ec36ad4f8e4f8be6cc3025ee24eafd1a01d296
Ejiroghene <ejay.efe@gmail.com>
2013-09-12 16:08:42 -0400

    update changes

--- js/jsconstruction4project2.js
+++ js/jsconstruction4project2.js
@@ -1,9 +1,9 @@
 window.addEventListener("DOMContentLoaded", function(){
-
+localStorage.key(0);
 
 			function $(x){
 					var theElement = document.getElementById(x);
-					return theElement
+					return theElement;
 			}
 			
 			// create select field elements and populate with options
@@ -34,7 +34,7 @@ window.addEventListener("DOMContentLoaded", function(){
 					if($("urgency").checked){
 					urgencyValue = $("urgency").value;
 					}else{
-						urgencyValue = "No"
+						urgencyValue = "No";
 						}
 			
 			
@@ -45,6 +45,8 @@ window.addEventListener("DOMContentLoaded", function(){
 					
 				getRadioActive();
 				getCheckboxActive();
+				positionValue;
+				urgencyValue ;
 					
 					var item 			= {};
 						item.agegrade	= ["Age Category:", $("agegrades").value];
@@ -52,7 +54,6 @@ window.addEventListener("DOMContentLoaded", function(){
 						item.lastname	= ["Last Name:", $("last-name").value]; 
 						item.email		= ["Agent Email:", $("email").value];  
 						item.phone		= ["Agent Phone:", $("phone").value];
-						item.email		= ["Agent Email:", $("email").value];
 						item.position 	= ["Field Position:", positionValue];
 						item.urgency 	= ["Urgent:", urgencyValue];
 						item.rating 	= ["Player Rating:", $("rating").value];
@@ -63,14 +64,49 @@ window.addEventListener("DOMContentLoaded", function(){
 				alert("Player Saved!");
 			}
 			
+			function getData(){
+			var makeDiv = document.createElement("div");
+			makeDiv.setAttribute("id", "items");
+			var makeList = document.createElement("ul");
+			makeDiv.appendChild(makeList);
+			document.body.appendChild(makeDiv);
+			for(var i=0, d=localStorage.length; i<d; i++){
+				var collectList = document.createElement("li");
+				makeList.appendChild(collectList);
+				
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				var dataObj = JSON.parse(value);
+				var makeSubList = document.createElement("ul");
+				collectList.appendChild(makeSubList);
+				
+				for(var n in dataObj){
+					var makeSubli = document.createElement("li");
+					makeSubList.appendChild(makeSubli);
+					var subText = dataObj[n][0]+" "+dataObj[n][1];
+					makeSubli.innerHTML = subText;
+					}
+				
+			}
+			
+		}
+			
 			var ageGrades = ["-- Age Category --", "U-17", "U-23", "Over 23"];
+			
+			
 			makeCart();
-			alert(localStorage.value(0));
+			
+			for (i=0, j=localStorage.length; i<j; i++){
+			var theKey = localStorage.key(i);
+			var theValue = localStorage.getItem(theKey);
+			console.log(theKey + ": " + theValue);
+}
 
-			/*var showAll = $("showAll");
-			showAll.addEventLister("click", getData);
-			var clearAll = $("clearAll");
-			clearAll.addEventLister("click", clearLocal);*/
+
+			var showAll = $("showAll");
+			showAll.addEventListener("click", getData);
+			/*var clearAll = $("clear");
+			clearAll.addEventListener("click", clearLocal);*/
 			var submit = $("submit");
 			submit.addEventListener("click", submitData);
 
